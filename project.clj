3476a6dfdca06bea108f461d175462afb5d30073
Thomas Greve Kristensen <tgk@cs.au.dk>
2010-05-21 08:57:37 +0200

    Added project.clj for lein

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject clj-todo "1.0.0-SNAPSHOT"
+  :description "A small lib for adding todo annotations to Clojure projects."
+  :dependencies [[org.clojure/clojure "1.1.0"]]
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])

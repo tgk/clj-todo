a62ca9269e24572f8badf701867487261d31d730
Thomas Greve Kristensen <tgk@cs.au.dk>
2010-05-25 21:54:05 +0200

    Copyright inside project.clj

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "A small lib for adding todo annotations to Clojure projects."
   :dependencies [[org.clojure/clojure "1.1.0"]
 		 [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]
+		     [autodoc "0.7.1"]]
+  :autodoc {:copyright "2010 Thomas G. Kristensen"})
